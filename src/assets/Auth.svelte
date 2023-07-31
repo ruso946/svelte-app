33a3c1100898a5c468a80f4d9bce3b0a4e0b2472
gmktdev <gmktdev@gmail.com>
2023-07-31 14:57:51 -0300

    delete user agregado al auth

--- src/assets/Auth.svelte
+++ src/assets/Auth.svelte
@@ -7,7 +7,17 @@
   import { createEventDispatcher } from "svelte";
   import { authenticatedUser } from "../store";
   import { auth } from "../configFirebase/firebasePacientes";
+  const dispatch = createEventDispatcher();
+  let liemail = "";
+  let lipassword = "";
+  
+  let email = "";
+  let password = "";
 
+  let demail = "";
+  let dpassword = "";
+
+  let mensajeError = "";
   const createUser = async () => {
     createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
@@ -16,30 +26,24 @@
         // ...
       })
       .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
+        mensajeError = error.message;
         // ..
       });
   };
-
-  const dispatch = createEventDispatcher();
-
-  let email = "";
-  let password = "";
 
   async function login() {
     try {
       const userCredential = await signInWithEmailAndPassword(
         auth,
-        email,
-        password,
+        liemail,
+        lipassword,
       );
       $authenticatedUser = userCredential.user; // Actualizamos el estado de autenticación con el usuario autenticado
-      console.log(userCredential);	  
+      console.log(userCredential);
       dispatch("loginSuccess", userCredential.user);
     } catch (error) {
       console.error("Error al iniciar sesión:", error.message);
-	  mensajeError = error.message;
+      mensajeError = error.message;
     }
   }
 
@@ -57,16 +61,43 @@
     console.log(authenticatedUser);
   }
 
-  let mensajeError = "";
+  async function deleteUser() {
+  try {
+	await signInWithEmailAndPassword(
+        auth,
+        demail,
+        dpassword,
+      );
+    await auth.currentUser.delete();
+    console.log('Cuenta eliminada correctamente.');
+  } catch (error) {
+    console.error('Error al eliminar la cuenta:', error.message);
+  }
+}
 </script>
 
 <main>
-  {#if $authenticatedUser} 	
+  {#if $authenticatedUser}
     <button on:click={logout}>cerrar sesion</button>
-	<p style="font-size: x-small">bienvenido {auth.currentUser.email}</p>    
+    <p style="font-size: x-small">bienvenido {auth.currentUser.email}</p>
   {:else}
     <h1>Iniciar sesión</h1>
     <form on:submit|preventDefault={login}>
+      <label>
+        Correo electrónico:
+        <input type="email" bind:value={liemail} required />
+      </label>
+      <label>
+        Contraseña:
+        <input type="password" bind:value={lipassword} required />
+      </label>
+      <button type="submit">Iniciar sesión</button>
+    </form>
+
+    <div id="mensaje-error">{mensajeError}</div>
+
+    <h1>Crear user</h1>
+    <form on:submit|preventDefault={createUser}>
       <label>
         Correo electrónico:
         <input type="email" bind:value={email} required />
@@ -75,9 +106,24 @@
         Contraseña:
         <input type="password" bind:value={password} required />
       </label>
-      <button type="submit">Iniciar sesión</button>
+      <button type="submit">crear User</button>
     </form>
 
-	<div id="mensaje-error">{mensajeError}</div>
+    <div id="mensaje-error">{mensajeError}</div>
+
+	<h1>Borrar cuenta</h1>
+    <form on:submit|preventDefault={deleteUser}>
+      <label>
+        Correo electrónico:
+        <input type="email" bind:value={demail} required />
+      </label>
+      <label>
+        Contraseña:
+        <input type="password" bind:value={dpassword} required />
+      </label>
+      <button type="submit">delete User</button>
+    </form>
+
+    <div id="mensaje-error">{mensajeError}</div>
   {/if}
 </main>
